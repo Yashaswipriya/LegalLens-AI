2d7eb7af56b940a5f6d5fe9af822438b585a79ff
Yashaswipriya <yashaswipulukuri@gmail.com>
2025-07-29 22:52:31 +0530

    upload error fixed

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middleware/authMiddleware');
 const contractRoutes = require('./routes/contractRoutes');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
 const app = express();
+
+const uploadPath = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
@@ -17,6 +24,10 @@ app.use('/api/me', authMiddleware, (req, res) => {
   res.json({ message: 'This is a protected route', user: req.user });
 });
 app.use('/api/contract', contractRoutes);
+
+app.get('/', (req, res) => {
+  res.send('LegalLens AI backend is running ✅');
+});
 
 // Start server
 const PORT = process.env.PORT || 5000;
